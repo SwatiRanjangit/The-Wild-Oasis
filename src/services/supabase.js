0b58d5733f69e367f3c93706bd4fd5f654f41ad1
Swati Ranjan <swatirp83@gmail.com>
2023-08-09 16:11:51 +0530

    connected successfully to supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://dysfrqyevghjjcljztne.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5c2ZycXlldmdoampjbGp6dG5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE1NjkyMzIsImV4cCI6MjAwNzE0NTIzMn0.XhJ2tACLsLxEVOf56g0z_RKE9B_iP-Xq3kjuhBYMz6M";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
