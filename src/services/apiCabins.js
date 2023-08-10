1ceb2c5cfbd94dab47486b4fbbb3f9f5fa1f9395
Swati Ranjan <swatirp83@gmail.com>
2023-08-10 18:00:43 +0530

    react query1

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,7 +4,17 @@ export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
   if (error) {
     console.error(error);
-    throw new Error("cabins could not be loaded");
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be deleted");
   }
 
   return data;
